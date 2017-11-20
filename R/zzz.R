ffa3eda87b84bfa50c263d3eea2babbe15feee6b
Robrecht Cannoodt <brechtus@gmail.com>
2017-11-20 18:20:56 +0100

    not sure why this is still an issue

---
+++ R/zzz.R
@@ -0,0 +1,9 @@
+# # This can be removed by adding ParamHelpers to DESCRIPTION
+.onLoad <- function(libname, pkgname){
+  packageStartupMessage("Loading discreteNameToValue in your global environment -- this is a dirty fix.")
+  assign(
+    "discreteNameToValue",
+    ParamHelpers::discreteNameToValue,
+    envir = .GlobalEnv
+  )
+}
