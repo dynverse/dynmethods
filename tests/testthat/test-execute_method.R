1923e85c0a07c0b4f01f37c2ef0764a14e8cd885
Robrecht Cannoodt <brechtus@gmail.com>
2017-12-05 15:05:54 +0100

    expand test

--- tests/testthat/test-execute_method.R
+++ tests/testthat/test-execute_method.R
@@ -188,20 +188,20 @@ test_that("Testing timeout functionality of execute_method with dummy wrapper", 
   }
 })
 
-# methods <- get_descriptions(as_tibble = FALSE)
-# for (method in methods) {
-#   test_that(pritt("Testing whether {method$short_name} is able to run on simple data"), {
-#   # print(method$short_name)
-#     params <- ParamHelpers::generateDesignOfDefaults(method$par_set) %>% ParamHelpers::dfRowToList(method$par_set, 1)
-#     toy <- dyntoy::generate_toy_datasets(trajectory_types = "linear", num_replicates = 1, num_cells = 50, num_genes = 31)
-#     out <- execute_method(toy, method, parameters = params, timeout = 100)
-#     error <- out[[1]]$summary$error[[1]]
-#     error
-#     expect_null(error)
-#     # if erroring:
-#     # list2env(params, globalenv())
-#     # counts <- toy$counts[[1]]
-#   })
-# }
+methods <- get_descriptions(as_tibble = FALSE)
+for (method in methods) {
+  test_that(pritt("Testing whether {method$short_name} is able to run on simple data"), {
+    params <- ParamHelpers::generateDesignOfDefaults(method$par_set, trafo = TRUE) %>% ParamHelpers::dfRowToList(method$par_set, 1)
+    toy <- dyntoy::generate_toy_datasets(trajectory_types = "linear", num_replicates = 1, num_cells = 50, num_genes = 101)
+    out <- execute_method(toy, method, parameters = params, timeout = 100)
+    error <- out[[1]]$summary$error[[1]]
+    error
+    expect_null(error)
+    # if erroring:
+    # list2env(params, globalenv())
+    # counts <- toy$counts[[1]]
+    # expression <- toy$expression[[1]]
+  })
+}
 
 # TODO: implement test for checking whether zerod columns work
