dca879787ffdaf380e3470ed25992ac17e254f57
Robrecht Cannoodt <brechtus@gmail.com>
2018-02-02 14:37:50 +0100

    disable testing all wrappers for now

--- tests/testthat/test-execute_method.R
+++ tests/testthat/test-execute_method.R
@@ -137,19 +137,3 @@ test_that("Testing prior passing for execute_method", {
   }
 })
 
-
-tasks <- dyntoy::toy_tasks %>% filter(model == "simple_linear") %>% slice(1)
-methods <- get_descriptions(as_tibble = FALSE)
-for (method in methods) {
-  test_that(pritt("Testing whether {method$short_name} is able to run on simple data"), {
-    params <- ParamHelpers::generateDesignOfDefaults(method$par_set, trafo = TRUE) %>% ParamHelpers::dfRowToList(method$par_set, 1)
-    out <- execute_method(tasks, method, parameters = params)
-    error <- out[[1]]$summary$error[[1]]
-    error
-    expect_null(error)
-    # if erroring:
-    # list2env(params, globalenv())
-    # counts <- toy$counts[[1]]
-    # expression <- toy$expression[[1]]
-  })
-}
