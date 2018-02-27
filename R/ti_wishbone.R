7aec4466e441a4f7d2b171989ba2204681011a08
Robrecht Cannoodt <brechtus@gmail.com>
2018-02-27 15:39:51 +0100

    add n_end_states optional prior to wishbone

--- R/ti_wishbone.R
+++ R/ti_wishbone.R
@@ -15,7 +15,8 @@ abstract_wishbone_description <- function(method) {
       makeIntegerParam(id = "k", lower = 2L, default = 15L, upper = 100L),
       makeIntegerParam(id = "num_waypoints", lower = 2L, default = 250L, upper = 500L),
       makeLogicalParam(id = "normalize", default = TRUE),
-      makeNumericParam(id = "epsilon", lower = 0.1, default = 1, upper = 10)
+      makeNumericParam(id = "epsilon", lower = 0.1, default = 1, upper = 10),
+      makeDiscreteParam(id = "method_name", values = c("wndrlst", "wishbone"), default = method, tunable = FALSE)
     ),
     properties = c(),
     run_fun = run_wishbone,
@@ -34,6 +35,7 @@ description_wndrlust <- abstract_wishbone_description("wndrlst")
 run_wishbone <- function(
   counts,
   start_cells,
+  n_end_states = NULL,
   marker_feature_ids = NULL,
   knn = 15,
   n_diffusion_components = 2,
@@ -42,24 +44,33 @@ run_wishbone <- function(
   k = 15,
   num_waypoints = 250,
   normalize = TRUE,
-  epsilon = 1
+  epsilon = 1,
+  method_name = "wishbone"
 ) {
   requireNamespace("Wishbone")
 
+  # reduce the number of waypoints if needed
   if (num_waypoints > nrow(counts) / 2) {
     new_num <- ceiling(nrow(counts) / 2)
     warning("Reducing the number of waypoints to the number of cells (originally = ", num_waypoints, ", now = ", new_num, ")")
     num_waypoints <- new_num
   }
 
+  # sample one start cell if more are given
   start_cell_id <- sample(start_cells, 1)
 
+  # transform marker features
   markers <-
     if (is.null(marker_feature_ids)) {
       "~"
     } else {
       marker_feature_ids
     }
+
+  # if the number of end states are given, derive branch parameter from n_end_states
+  if (method_name == "wishbone" && !is.null(n_end_states)) {
+    branch <- n_end_states > 1
+  }
 
   # TIMING: done with preproc
   tl <- add_timing_checkpoint(NULL, "method_afterpreproc")
