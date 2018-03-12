2d9528b4972f79b2b464838ebe68dcea6cacf916
Robrecht Cannoodt <brechtus@gmail.com>
2018-03-12 13:03:45 +0100

    clean up get_descriptions function

--- R/wrapper_get_descriptions.R
+++ R/wrapper_get_descriptions.R
@@ -6,11 +6,10 @@
 #' @export
 get_descriptions <- function(as_tibble = TRUE) {
   requireNamespace("dynmethods")
-  functions <- lsf.str(asNamespace("dynmethods"))
-  description_functions <- functions[grep("description_", functions)]
-  descriptions <- lapply(description_functions, function(fun_name) {
-    do.call(fun_name, args = list(), envir = asNamespace("dynmethods"))
-  })
+
+  descriptions <- lsf.str(asNamespace("dynmethods"), pattern = "description_") %>%
+    map(~ do.call(., args = list(), envir = asNamespace("dynmethods")))
+
   if (as_tibble) {
     list_as_tibble(descriptions)
   } else {
