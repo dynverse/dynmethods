4dd206a329979098a8f7a7ab41fc87ed861935a3
Robrecht Cannoodt <brechtus@gmail.com>
2018-03-22 13:21:54 +0100

    Rename acos to atan

--- R/ti_atan.R
+++ R/ti_atan.R
@@ -1,19 +1,19 @@
-#' Description for acos
+#' Description for atan
 #' @export
-description_acos <- function() create_description(
-  name = "Arc cosine",
-  short_name = "acos",
+description_atan <- function() create_description(
+  name = "Arc-tangent",
+  short_name = "atan",
   package_loaded = c(""),
   package_required = c(),
   par_set = makeParamSet(
     makeDiscreteParam(id = "dimred", default = "pca", values = names(list_dimred_methods()))
   ),
   properties = c(),
-  run_fun = run_acos,
-  plot_fun = plot_acos
+  run_fun = run_atan,
+  plot_fun = plot_atan
 )
 
-run_acos <- function(
+run_atan <- function(
   expression,
   dimred = "pca"
 ) {
@@ -23,7 +23,8 @@ run_acos <- function(
   # perform PCA dimred
   space <- dimred(expression, method = dimred, ndim = 2)
 
-  pseudotimes <- acos(dimred[,1] / sqrt(rowSums(dimred^2))) / (2 * pi)
+  # transform to pseudotimes using atan2
+  pseudotimes <- atan2(dimred[,2], dimred[,1]) / 2 / pi
 
   # TIMING: done with method
   tl <- tl %>% add_timing_checkpoint("method_aftermethod")
@@ -42,7 +43,7 @@ run_acos <- function(
 }
 
 #' @importFrom viridis scale_colour_viridis
-plot_acos <- function(prediction) {
+plot_atan <- function(prediction) {
   dimred_df <- data.frame(prediction$dimred, pseudotime = cos(prediction$pseudotimes * 2 * pi))
   g <- ggplot() +
     geom_point(aes(PC1, PC2, colour = pseudotime), dimred_df) +
