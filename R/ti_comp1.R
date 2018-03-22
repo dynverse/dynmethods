791e514a864a717d8c0c4489f848eb340f12c13b
Robrecht Cannoodt <brechtus@gmail.com>
2018-03-22 15:40:10 +0100

    comp1 does not have an ndim parameter

--- R/ti_comp1.R
+++ R/ti_comp1.R
@@ -20,7 +20,7 @@ run_compone <- function(
   # TIMING: done with preproc
   tl <- add_timing_checkpoint(NULL, "method_afterpreproc")
 
-  space <- dimred(expression, method = dimred, ndim = ndim)
+  space <- dimred(expression, method = dimred, ndim = 2)
 
   # TIMING: done with method
   tl <- tl %>% add_timing_checkpoint("method_aftermethod")
