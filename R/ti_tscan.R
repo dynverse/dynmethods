58fb60ab0e0f1cca1365a448db73643b85773862
Wouter Saelens <wouter.saelens@gmail.com>
2018-04-04 16:00:00 +0200

    TSCAN milestone names to "MX" format

--- R/ti_tscan.R
+++ R/ti_tscan.R
@@ -96,7 +96,7 @@ plot_tscan <- function(prediction) {
 
   g <- ggplot() +
     geom_point(aes(PC1, PC2, colour = label, shape = label), space) +
-    geom_text(aes(PC1, PC2, label = clus_id), prediction$dimred_milestones %>% as.data.frame %>% rownames_to_column("clus_id"), size = 6) +
+    geom_text(aes(PC1, PC2, label = clus_id), prediction$dimred_milestones %>% as.data.frame %>% rownames_to_column("clus_id") %>% mutate(clus_id = paste0("M", row_number())), size = 6) +
     geom_segment(aes(x = from_PC1, xend = to_PC1, y = from_PC2, yend = to_PC2), prediction$dimred_trajectory_segments %>% as.data.frame()) +
     theme(legend.position = "none")
   process_dynplot(g, prediction$id)
