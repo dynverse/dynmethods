a412d157febfb39c56868c3006b607915157b056
Robrecht Cannoodt <brechtus@gmail.com>
2018-04-18 10:02:35 +0200

    clean up script

--- .travis_install_python.sh
+++ .travis_install_python.sh
@@ -1,7 +1,6 @@
 bucket="travis-python-archives"
 vers="3.6"
 lang="python"
-ext="bz2"
 PYENV_PATH_FILE="/etc/profile.d/pyenv.sh"
 archive_basename="${lang}-${vers}"
 archive_filename="${archive_basename}.tar.bz2"
@@ -13,6 +12,5 @@ echo "Downloading archive: ${archive_url}"
 curl -sSf -o ${archive_filename} ${archive_url}
 sudo tar xjf ${archive_filename} --directory /
 rm ${archive_filename}
-
 echo 'export PATH=/opt/python/${vers}/bin:$PATH' | sudo tee -a ${PYENV_PATH_FILE} &>/dev/null
-export PATH=/opt/python/${vers}/bin:$PATH
+export PATH="/opt/python/${vers}/bin:$PATH""
