3123982dbe0f8c1403c2b5ff50f73ed25fbab9d3
Wouter Saelens <wouter.saelens@gmail.com>
2018-05-07 12:47:47 +0200

    reexport %<-%

--- R/wrapper_docker.R
+++ R/wrapper_docker.R
@@ -1,3 +1,6 @@
+#' Stop the dynmethods docker
+#'
+#' @export
 stop_dynmethods_docker <- function() {
   to_stop <- system("docker ps -a | grep dynmethods | awk '{print $1}'", intern=T)
 
@@ -7,13 +10,18 @@ stop_dynmethods_docker <- function() {
   }
 }
 
+#' Start the dynmethods docker
+#'
+#' Make sure docker is installed and available through path
+#'
+#' @export
 start_dynmethods_docker <- function() {
   stop_dynmethods_docker()
 
   cl <- future::makeClusterPSOCK(
     "localhost",
     rscript = c(
-      "docker", "run", "--network=host", "--name=dynmethods", "dynmethods",
+      "docker", "run", "--network=host", "--name=dynmethods", "dynverse/dynmethods",
       "Rscript"
     ),
     rscript_args = c(
@@ -23,3 +31,7 @@ start_dynmethods_docker <- function() {
   )
   future::plan(future::cluster, workers = cl)
 }
+
+#' @importFrom future %<-%
+#' @export
+future::`%<-%`
