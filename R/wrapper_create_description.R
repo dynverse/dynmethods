719fd62944c3341f4106483c0c05a8effffe5abb
Robrecht Cannoodt <brechtus@gmail.com>
2018-05-22 17:52:12 +0200

    remove override_runfun_params in favour for run_fun_defaults

--- R/wrapper_create_description.R
+++ R/wrapper_create_description.R
@@ -8,8 +8,8 @@
 #' @param properties Several descriptive properties of the method. WIP.
 #' @param run_fun A function to run the TI, needs to have 'counts' as its first param.
 #' @param plot_fun A function to plot the results of a TI, needs to have 'prediction' as its first param.
-#' @param override_runfun_params Whether or not to override the default parameters
-#' of `run_fun` with those described in `par_set`.
+#'   of `run_fun` with those described in `par_set`.
+#' @param run_fun_defaults A named list with which to override the default parameters of the \code{run_fun}
 create_description <- function(
   name,
   short_name,
@@ -19,19 +19,18 @@ create_description <- function(
   properties,
   run_fun,
   plot_fun,
-  override_runfun_params = TRUE
+  run_fun_defaults
 ) {
-  if (override_runfun_params) {
-    default_params <- par_set %>%
-      generateDesignOfDefaults(trafo = TRUE) %>%
-      ParamHelpers::dfRowToList(par_set, 1)
+  default_params <- par_set %>%
+    generateDesignOfDefaults(trafo = TRUE) %>%
+    ParamHelpers::dfRowToList(par_set, 1)
 
-    if(!all(names(default_params) %in% formalArgs(run_fun))) {
-      stop("Not all default params described in par_set are listed in the run_fun.")
-    }
-
-    formals(run_fun)[names(default_params)] <- default_params
+  if(!all(names(default_params) %in% formalArgs(run_fun))) {
+    stop("Not all default params described in par_set are listed in the run_fun.")
   }
+
+  formals(run_fun)[names(default_params)] <- default_params
+
   desc <- lst(
     name,
     short_name,
