f5122c72b2f192c31cbafb35882ded121e25cdc9
Robrecht Cannoodt <brechtus@gmail.com>
2018-05-24 15:36:43 +0200

    fix get_ti_methods

--- R/wrapper_get_descriptions.R
+++ R/wrapper_get_descriptions.R
@@ -7,7 +7,7 @@
 get_ti_methods <- function(as_tibble = TRUE) {
   requireNamespace("dynmethods")
 
-  ti_methods <- lsf.str(asNamespace("dynmethods"), pattern = "ti_") %>%
+  ti_methods <- lsf.str(asNamespace("dynmethods"), pattern = "^ti_") %>%
     map(~ do.call(., args = list(), envir = asNamespace("dynmethods")))
 
   if (as_tibble) {
