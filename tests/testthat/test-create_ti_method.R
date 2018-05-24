d04c5c17beebef9c88c0f7b6af9f9ef9775c8c02
Robrecht Cannoodt <brechtus@gmail.com>
2018-05-24 15:39:27 +0200

    fix create_ti_method test

--- tests/testthat/test-create_ti_method.R
+++ tests/testthat/test-create_ti_method.R
@@ -10,20 +10,23 @@ test_that("Testing create_ti_method with dummy method", {
     par_set = ParamHelpers::makeParamSet(
       ParamHelpers::makeDiscreteParam(id = "param", default = "banana", values = c("apple", "banana", "cherry"))
     ),
-    properties = c("space", "trajectory"),
     run_fun = function(counts, param = "fjioiw") param,
-    plot_fun = function(out) "cake",
-    run_fun_defaults = list(param = "101010")
+    plot_fun = function(out) "cake"
   )
-  expect_equal( dummy$name, "dummy 1" )
-  expect_equal( dummy$short_name, "dum1" )
-  expect_equal( dummy$package_loaded, "dynverse" )
-  expect_equal( dummy$package_required, "tidyverse" )
-  expect_is( dummy$par_set, "ParamSet" )
-  expect_equal( dummy$properties, c("space", "trajectory") )
-  expect_is( dummy$run_fun, "function" )
+
+  dummy_instance <- dummy()
+
+  expect_equal( dummy_instance$name, "dummy 1" )
+  expect_equal( dummy_instance$short_name, "dum1" )
+  expect_equal( dummy_instance$package_loaded, "dynverse" )
+  expect_equal( dummy_instance$package_required, "tidyverse" )
+  expect_is( dummy_instance$par_set, "ParamSet" )
+  expect_is( dummy_instance$run_fun, "function" )
   # take into account parameter overwriting by parmamset
-  expect_equal( dummy$run_fun(NULL), "101010" )
-  expect_is( dummy$plot_fun, "function" )
-  expect_equal( dummy$plot_fun(NULL), "cake" )
+  expect_equal( dummy_instance$run_fun(NULL), "banana" )
+  expect_is( dummy_instance$plot_fun, "function" )
+  expect_equal( dummy_instance$plot_fun(NULL), "cake" )
+
+  dummy_instance2 <- dummy(param = "101010")
+  expect_equal( dummy_instance2$run_fun(NULL), "101010" )
 })
