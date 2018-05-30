850e8dd4de8b8ceb6e60af15e4dd9839c0efd92c
zouter <wouter.saelens@gmail.com>
2018-05-30 16:56:39 +0200

    include map2

--- R/package.R
+++ R/package.R
@@ -11,7 +11,7 @@
 #' @importFrom dynplot process_dynplot plot_default
 #' @importFrom stats cor dist kmeans median prcomp quantile runif setNames step time
 #' @importFrom utils installed.packages head tail
-#' @importFrom purrr %>% map map_df map_chr map_lgl map_int map_dbl keep discard set_names pmap
+#' @importFrom purrr %>% map map_df map_chr map_lgl map_int map_dbl keep discard set_names pmap map2
 #' @importFrom magrittr %<>% %$% set_colnames set_rownames
 #' @importFrom lhs randomLHS
 # lhs is needed for ParamHelpers generateDesign
