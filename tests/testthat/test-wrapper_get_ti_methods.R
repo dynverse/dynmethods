c88fff65ab6d9c093c70a09cb915b87e499b6dd5
zouter <wouter.saelens@gmail.com>
2018-06-13 15:56:56 +0200

    fix get_ti_methods test

--- tests/testthat/test-wrapper_get_ti_methods.R
+++ tests/testthat/test-wrapper_get_ti_methods.R
@@ -7,18 +7,12 @@ if (Sys.getenv("TRAVIS") != "true") {
 
     lis <- dynwrap::get_ti_methods(as_tibble=FALSE, packages="dynmethods")
     expect_that(lis, is_a("list"))
-
-    for (descr in lis) {
-      test_that(paste0("Description ", descr$name), {
-        expect_match(descr$short_name, "^[a-z0-9_]*$")
-      })
-    }
   })
 
   methods <- get_ti_methods(packages="dynmethods")
 
   for (i in seq_len(nrow(methods))) {
-    method <- extract_row_to_list(methods, i)
+    method <- extract_row_to_list(methods, i)$method_func()
 
     test_that(pritt("Checking whether {method$short_name} can generate parameters"), {
       par_set <- method$par_set
