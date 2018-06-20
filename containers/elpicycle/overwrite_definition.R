acb1044c97288b12bf14aa89ae8ca65e7c5d4cb7
zouter <wouter.saelens@gmail.com>
2018-06-20 21:21:36 +0200

    update several method's authors & names

--- containers/elpicycle/overwrite_definition.R
+++ containers/elpicycle/overwrite_definition.R
@@ -1,7 +1,8 @@
 library(yaml)
 
 definition <- read_yaml("/code/definition.yml")
-definition$name = "elpicycle"
+definition$name = "ElPiGraph cyclic"
+definition$short_name = "elpicycle"
 definition$parameters$topology$default <- "cycle"
 definition$parameters$topology$values <- "cycle"
 definition$input$optional <- NULL
--- containers/elpilinear/overwrite_definition.R
+++ containers/elpilinear/overwrite_definition.R
@@ -1,7 +1,8 @@
 library(yaml)
 
 definition <- read_yaml("/code/definition.yml")
-definition$name = "elpilinear"
+definition$name = "ElPiGraph linear"
+definition$short_name = "elpilinear"
 definition$parameters$topology$default <- "linear"
 definition$parameters$topology$values <- "linear"
 definition$input$optional <- NULL
