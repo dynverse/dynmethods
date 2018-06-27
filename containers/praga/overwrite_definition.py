8ac2c0fc0cdab5ab2c5ca35603a03110e76aa32c
zouter <wouter.saelens@gmail.com>
2018-06-27 17:25:59 +0200

    fix praga with new paga

--- containers/praga/overwrite_definition.py
+++ containers/praga/overwrite_definition.py
@@ -3,6 +3,6 @@ from yaml import load, dump
 definition = load(open("/code/definition.yml", "r"))
 definition["name"] = "projected PAGA"
 definition["short_name"] = "praga"
-definition["output"]["outputs"] = ["dimred_projection", "timings"]
+definition["output"]["outputs"] = ["dimred_projection", "dimred", "timings"]
 
 dump(definition, open("/code/definition.yml", "w"))
