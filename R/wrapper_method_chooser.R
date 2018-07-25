54a7030ea16be59e72ced8aa3f0bcebfa9e8d3ab
Robrecht Cannoodt <rcannood@gmail.com>
2018-07-25 14:08:07 +0200

    rework data-raw script to build containers

--- R/wrapper_method_chooser.R
+++ R/wrapper_method_chooser.R
@@ -21,3 +21,22 @@ create_ti_method_chooser <- function(method_function, docker_container) {
 
   func
 }
+
+
+create_ti_method <- function(docker_repository, run_environment = NULL, ...) {
+  if (is.null(run_environment)) {
+    run_environment <- getOption("dynwrap_run_environment")
+  }
+  if (is.null(run_environment)) {
+    run_environment <- "docker"
+  }
+
+  # choose environments
+  if (run_environment == "docker") {
+    method <- create_docker_ti_method(docker_repository)
+  } else if (run_environment == 'singularity') {
+    method <- create_singularity_ti_method(docker_repository, ".simg")
+  }
+
+  method(...)
+}
