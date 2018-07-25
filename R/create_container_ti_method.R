450903cf0b2a72759687e1abaa80ac8322c7a8f9
Robrecht Cannoodt <rcannood@gmail.com>
2018-07-25 14:52:35 +0200

    add documentation for create_container_ti_method

--- R/create_container_ti_method.R
+++ R/create_container_ti_method.R
@@ -4,6 +4,14 @@
 # * create_docker_ti_method
 # * create_singularity_ti_method
 # * create_ti_method
+
+#' Creating a TI method from a docker repository
+#'
+#' Supports both docker and singularity as a backend.
+#'
+#' @param docker_repository The id of the docker repository.
+#' @param run_environment In which environment to run the method, can be `"docker"` or `"singularity"`.
+#' @param ... Default parameters for the method.
 create_container_ti_method <- function(docker_repository, run_environment = NULL, ...) {
   if (is.null(run_environment)) {
     run_environment <- getOption("dynwrap_run_environment")
