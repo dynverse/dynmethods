b29625842949410455cf42ca810d7a23ebe1256f
Robrecht Cannoodt <rcannood@gmail.com>
2018-07-25 16:34:53 +0200

    find the missing TI methods

--- data-raw/3-generate_methods_data.R
+++ data-raw/3-generate_methods_data.R
@@ -1,10 +1,11 @@
 library(tidyverse)
 
 # generate methods object
-methods <- dynwrap::get_ti_methods(as_tibble = FALSE) %>%
+methods <- dynwrap::get_ti_methods(as_tibble = FALSE, ti_packages = "dynmethods") %>%
   map(function(method) {
     method$method_func() %>% discard(is.function)
   }) %>%
   dynutils::list_as_tibble()
 
-usethis::use_data(methods, overwrite = TRUE)
+devtools::use_data(methods, overwrite = TRUE)
+devtools::document()
