4bf7bf5322e6fedc52dd59a36c0d8a94a9cceee7
Robrecht Cannoodt <rcannood@gmail.com>
2018-07-26 13:37:39 +0200

    update angle

--- containers/angle/run.R
+++ containers/angle/run.R
@@ -12,41 +12,41 @@ library(purrr)
 data <- read_rds("/input/data.rds")
 params <- jsonlite::read_json("/input/params.json")
 
+#' @examples
+#' data <- dyntoy::generate_dataset(model = "cyclic") %>% c(., .$prior_information)
+#' params <- yaml::read_yaml("containers/angle/definition.yml")$parameters %>%
+#'   {.[names(.) != "forbidden"]} %>%
+#'   map(~ .$default)
+
 #   ____________________________________________________________________________
 #   Infer trajectory                                                        ####
 
-run_fun <- function(
-  expression,
-  dimred = "pca"
-) {
-  # TIMING: done with preproc
-  tl <- add_timing_checkpoint(NULL, "method_afterpreproc")
+expression <- data$expression
 
-  # perform PCA dimred
-  space <- dyndimred::dimred(expression, method = dimred, ndim = 2)
 
-  # transform to pseudotime using atan2
-  pseudotime <- atan2(space[,2], space[,1]) / 2 / pi + .5
+# TIMING: done with preproc
+tl <- add_timing_checkpoint(NULL, "method_afterpreproc")
 
-  # TIMING: done with method
-  tl <- tl %>% add_timing_checkpoint("method_aftermethod")
+# perform PCA dimred
+space <- dyndimred::dimred(expression, method = params$dimred, ndim = 2)
 
-  # return output
-  wrap_prediction_model(
-    cell_ids = rownames(expression)
-  ) %>% add_cyclic_trajectory(
-    pseudotime = pseudotime,
-    do_scale_minmax = FALSE
-  ) %>% add_dimred(
-    dimred = space
-  ) %>% add_timings(
-    timings = tl %>% add_timing_checkpoint("method_afterpostproc")
-  )
-}
+# transform to pseudotime using atan2
+pseudotime <- atan2(space[,2], space[,1]) / 2 / pi + .5
 
-args <- params[intersect(names(params), names(formals(run_fun)))]
+# TIMING: done with method
+tl <- tl %>% add_timing_checkpoint("method_aftermethod")
 
-model <- do.call(run_fun, c(args, data))
+# return output
+model <- wrap_prediction_model(
+  cell_ids = rownames(expression)
+) %>% add_cyclic_trajectory(
+  pseudotime = pseudotime,
+  do_scale_minmax = FALSE
+) %>% add_dimred(
+  dimred = space
+) %>% add_timings(
+  timings = tl %>% add_timing_checkpoint("method_afterpostproc")
+)
 
 #   ____________________________________________________________________________
 #   Save output                                                             ####
