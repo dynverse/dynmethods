4cdf25c9d7eec68ff5f737bb9dd7f7da9414967d
Robrecht Cannoodt <rcannood@gmail.com>
2018-07-31 19:07:10 +0200

    add comments

--- data-raw/1-build_containers.R
+++ data-raw/1-build_containers.R
@@ -7,19 +7,25 @@ definition_files <- list.files("containers", pattern = "definition.yml", recursi
 
 # rebuild all dockers in the 'containers' folder
 out <- future_map(definition_files, function(file) {
+  # read definition
   definition <- yaml::read_yaml(file)
   docker_repo <- definition$docker_repository
   folder <- stringr::str_replace(file, "/definition.yml$", "")
 
   # processx::run("docker", args = c("pull", docker_repo), echo = TRUE)
+
   processx::run("docker", args = c("build", folder, "-t", docker_repo), echo = TRUE)
 
   tryCatch({
+    # read example data and possible parameters
     rm(data, params)
     source(stringr::str_replace(file, "/definition.yml$", "/example.R"))
+    params <- if (exists("params")) params else NULL
 
-    traj <- dynwrap::infer_trajectory(data, definition$id, parameters = {if (exists("params")) params else NULL}, verbose = FALSE)
+    # run method on example, with possible parameters
+    traj <- dynwrap::infer_trajectory(data, definition$id, parameters = params, verbose = FALSE)
 
+    # if traj is indeed a trajectory, push the docker to dockerhub
     if (dynwrap::is_wrapper_with_trajectory(traj)) {
       processx::run("docker", args = c("push", docker_repo), echo = TRUE)
 
