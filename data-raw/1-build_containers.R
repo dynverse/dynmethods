d7f8df1c809d13e4a32bf5b1e73454e425555f7d
Robrecht Cannoodt <rcannood@gmail.com>
2018-07-31 19:05:39 +0200

    test containers with examples when building the containers

--- data-raw/1-build_containers.R
+++ data-raw/1-build_containers.R
@@ -5,22 +5,29 @@ plan(multiprocess)
 
 definition_files <- list.files("containers", pattern = "definition.yml", recursive = TRUE, full.names = TRUE)
 
-#' @examples
-#' method <- "angle"
-#' file <- paste0("containers/", method, "/definition.yml")
-
 # rebuild all dockers in the 'containers' folder
-future_map(definition_files, function(file) {
+out <- future_map(definition_files, function(file) {
   definition <- yaml::read_yaml(file)
   docker_repo <- definition$docker_repository
   folder <- stringr::str_replace(file, "/definition.yml$", "")
 
   # processx::run("docker", args = c("pull", docker_repo), echo = TRUE)
   processx::run("docker", args = c("build", folder, "-t", docker_repo), echo = TRUE)
-  processx::run("docker", args = c("push", docker_repo), echo = TRUE)
-})
 
-#' @examples
-#' data <- dyntoy::generate_dataset(unique_id = "test", num_cells = 300, num_features = 300, model = "binary_tree")
-#' traj <- dynwrap::infer_trajectory(data, method, verbose = TRUE)
-#' dynplot::plot_graph(traj)
+  tryCatch({
+    rm(data, params)
+    source(stringr::str_replace(file, "/definition.yml$", "/example.R"))
+
+    traj <- dynwrap::infer_trajectory(data, definition$id, parameters = {if (exists("params")) params else NULL}, verbose = FALSE)
+
+    if (dynwrap::is_wrapper_with_trajectory(traj)) {
+      processx::run("docker", args = c("push", docker_repo), echo = TRUE)
+
+      data_frame(id = definition$id, built = TRUE, example = TRUE)
+    } else {
+      stop("No trajectory found")
+    }
+  }, error = function(e) {
+    data_frame(id = definition$id, built = TRUE, example = FALSE)
+  })
+})
