5cf78745d93e096536b1e71d7ea7790a99a01e3f
Robrecht Cannoodt <rcannood@gmail.com>
2018-08-03 14:38:40 +0200

    cd to tmp to allow pyscuba to create files

--- containers/scuba/run.py
+++ containers/scuba/run.py
@@ -14,6 +14,7 @@ import pandas as pd
 import time
 checkpoints = {}
 
+os.chdir("/tmp")
 
 #   ____________________________________________________________________________
 #   Load data                                                               ####
