a743ad64df3130da97f5dbba9665d0266dca3e75
Robrecht Cannoodt <rcannood@gmail.com>
2018-08-06 17:03:30 +0200

    fix example names

--- containers/projected_gng/example.R
+++ containers/projected_gng/example.R
@@ -1,6 +1,6 @@
 set.seed(1)
 data <- dyntoy::generate_dataset(
-  id = "gng_example",
+  id = "projected_gng_example",
   num_cells = 99,
   num_features = 101,
   model = "tree"
--- containers/projected_tscan/example.R
+++ containers/projected_tscan/example.R
@@ -1,6 +1,6 @@
 set.seed(1)
 data <- dyntoy::generate_dataset(
-  id = "tscan_example",
+  id = "projected_tscan_example",
   num_cells = 99,
   num_features = 101,
   model = "tree"
