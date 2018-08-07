6a8881bfd99a9d71b59d8ed996cee82e8346d158
Robrecht Cannoodt <rcannood@gmail.com>
2018-08-07 14:50:09 +0200

    don't use dimred projection for DPT

--- containers/dpt/run.R
+++ containers/dpt/run.R
@@ -12,7 +12,7 @@ data <- read_rds("/input/data.rds")
 params <- jsonlite::read_json("/input/params.json")
 
 #' @examples
-#' data <- dyntoy::generate_dataset(model = "bifurcating") %>% c(., .$prior_information)
+#' data <- dyntoy::generate_dataset(model = "tree") %>% c(., .$prior_information)
 #' params <- yaml::read_yaml("containers/dpt/definition.yml")$parameters %>%
 #'   {.[names(.) != "forbidden"]} %>%
 #'   map(~ .$default)
@@ -47,11 +47,16 @@ dm <- destiny::DiffusionMap(
 )
 
 # run DPT
-dpt_params <- lst(dm, w_width = params$w_width)
 if (!is.null(data$start_cell)) {
-  dpt_params$tips <- which(rownames(expression) %in% start_cell)
+  tips <- which(rownames(expression) %in% start_cell)
+} else {
+  tips <- destiny::random_root(dm)
 }
-dpt <- do.call(destiny::DPT, dpt_params)
+dpt <- destiny::DPT(
+  dm,
+  w_width = params$w_width,
+  tips = tips
+)
 
 # find DPT tips
 tips <- destiny::tips(dpt)
@@ -60,33 +65,70 @@ tip_names <- rownames(expression)[tips]
 # TIMING: done with method
 checkpoints$method_aftermethod <- as.numeric(Sys.time())
 
-# retrieve dimred
-dimred_cells <- dpt@dm@eigenvectors %>% magrittr::set_rownames(rownames(expression)) %>% as.matrix
+cell_ids <- rownames(expression)
 
-# get cluster assignment
+# construct grouping
 grouping <- dpt@branch[,1] %>%
   ifelse(is.na(.), 0, .) %>%
-  as.character()
-branches <- sort(unique(grouping))
+  as.character() %>%
+  paste0("Tip", .) %>%
+  set_names(cell_ids)
 
-# calculate cluster medians
-dimred_milestones <- t(sapply(branches, function(br) colMeans(dimred_cells[grouping == br,,drop=F])))
+group_ids <- sort(unique(grouping))
 
-# create star network
-milestone_network <- data_frame(
-  from = "0",
-  to = setdiff(branches, "0"),
-  length = sqrt(rowMeans((dimred_milestones[from,] - dimred_milestones[to,])^2)),
-  directed = TRUE
+# collect distances from tips
+tip_dists <- dpt[,tips] %>%
+  magrittr::set_colnames(., paste0("Tip", seq_len(ncol(.)))) %>%
+  magrittr::set_rownames(cell_ids)
+
+# calculate progressions
+outs <- map(
+  group_ids,
+  function(gid) {
+    cat("Processing ", gid, "\n", sep = "")
+    cixs <- which(grouping == gid)
+    cids <- cell_ids[cixs]
+    if (length(cids) > 0) {
+      if (gid == "Tip0") {
+        progr <- data_frame(
+          cell_id = cids,
+          from = gid,
+          to = sample(setdiff(group_ids, gid), length(cids), replace = TRUE),
+          percentage = 0
+        )
+        list(progr = progr, milnet = NULL)
+      } else {
+        # calculate min dist of gid to all other cells
+        max_range <- min(tip_dists[-cixs, gid])
+
+        # calculate percentage value of cells in gid
+        percentage <- 1 - pmin(tip_dists[cixs, gid] / max_range, 1)
+        progr <- data_frame(cell_id = cids, from = "Tip0", to = gid, percentage = percentage)
+        milnet <- data_frame(from = "Tip0", to = gid, length = max_range, directed = FALSE)
+        list(progr = progr, milnet = milnet)
+      }
+    } else {
+      list()
+    }
+  }
 )
+progressions <- map_df(outs, ~ .$progr)
+milestone_network <- map_df(outs, ~ .$milnet)
 
+# collect dimred
+dimred <- dm@eigenvectors %>%
+  magrittr::set_colnames(., paste0("Comp", seq_len(ncol(.)))) %>%
+  magrittr::set_rownames(cell_ids)
+
+# return output
 output <- lst(
-  cell_ids = rownames(dimred_cells),
+  cell_ids,
+  milestone_ids = group_ids,
   milestone_network,
-  dimred_milestones,
-  dimred = dimred_cells,
-  grouping = grouping,
-  tips,
+  progressions,
+  group_ids,
+  grouping,
+  dimred,
   timings = checkpoints
 )
 
