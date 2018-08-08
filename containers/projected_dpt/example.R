cb3836864ed80ffd3edba31fc60c4dc18669c803
Robrecht Cannoodt <rcannood@gmail.com>
2018-08-08 09:58:55 +0200

    fix example for projected_dpt

--- containers/projected_dpt/example.R
+++ containers/projected_dpt/example.R
@@ -1,6 +1,6 @@
 set.seed(1)
 data <- dyntoy::generate_dataset(
-  id = "specific_example/dpt",
+  id = "specific_example/projected_dpt",
   num_cells = 99,
   num_features = 101,
   model = "bifurcating"
