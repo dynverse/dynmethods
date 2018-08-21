9e1ab522766275b2c15f5506dc3d612581537aee
Robrecht Cannoodt <rcannood@gmail.com>
2018-08-21 10:22:01 +0200

    rename method_func to just fun

--- data-raw/3-generate_methods_data.R
+++ data-raw/3-generate_methods_data.R
@@ -3,7 +3,7 @@ library(tidyverse)
 # generate methods object
 devtools::install(dependencies = FALSE)
 methods <- dynwrap::get_ti_methods(ti_packages = "dynmethods", evaluate = TRUE) %>%
-  select(-method_func)
+  select(-fun)
 
 devtools::use_data(methods, overwrite = TRUE)
 devtools::document()
