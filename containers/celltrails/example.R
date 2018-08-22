780895fccb9af923ed1109fc14d1b313e09839a3
Robrecht Cannoodt <rcannood@gmail.com>
2018-08-22 19:01:53 +0200

    fix celltrails example

--- containers/celltrails/example.R
+++ containers/celltrails/example.R
@@ -1,8 +1,8 @@
 set.seed(1)
 data <- dyntoy::generate_dataset(
   id = "specific_example/celltrails",
-  num_cells = 99,
-  num_features = 101,
+  num_cells = 500,
+  num_features = 300,
   model = "tree"
 )
 params <- list()
