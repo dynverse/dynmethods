162838aefb45c9c3170176192d371cf2a85a5444
Wouter Saelens <wouter.saelens@gmail.com>
2019-06-27 15:22:42 +0200

    don't test R installation for now

--- tests/testthat/test-method.R
+++ tests/testthat/test-method.R
@@ -26,15 +26,15 @@ test_that("A method can be used with docker", {
 #   trajectory <- infer_trajectory(dataset, method)
 # })
 
-test_that("A method can be used with R", {
-  dataset <- dynwrap::example_dataset
-
-  babelwhale::test_docker_installation(detailed = TRUE)
-
-  config <- babelwhale::create_docker_config()
-  babelwhale::set_default_config(config)
-
-  method <- ti_scorpius(maxit = 0)
-
-  trajectory <- infer_trajectory(dataset, method)
-})
+# test_that("A method can be used with R", {
+#   dataset <- dynwrap::example_dataset
+#
+#   babelwhale::test_docker_installation(detailed = TRUE)
+#
+#   config <- babelwhale::create_docker_config()
+#   babelwhale::set_default_config(config)
+#
+#   method <- ti_scorpius(maxit = 0)
+#
+#   trajectory <- infer_trajectory(dataset, method)
+# })
